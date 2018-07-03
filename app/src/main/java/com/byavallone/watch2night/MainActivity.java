585c6231db5dfb063a91f6a00a8ba9b003e7b348
Eduardo Avallone de Souza <edu.ava@gmail.com>
2018-07-03 01:33:07 -0300

    Removing ConstraintLayout and adding GridView instead

--- app/src/main/java/com/byavallone/watch2night/MainActivity.java
+++ app/src/main/java/com/byavallone/watch2night/MainActivity.java
@@ -2,6 +2,7 @@ package com.byavallone.watch2night;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView mainGrid = (GridView) findViewById(R.id.main_gridview);
     }
 }
